3fdf3243cb7cb89458e8f9a650209351974e2324
my78kill <aigirlfriend143you@gmail.com>
2026-03-03 22:26:59 +0530

    Update badwords_hindi.py

--- badwords_hindi.py
+++ badwords_hindi.py
@@ -1,6 +1,7 @@
 HINDI_BAD_WORDS = [
-    "madarch", "bhench", "bhosd", "chut", "chud", "chod",
+    "madarch", "bhench", "bhosd", "c", "bsdk", "mkc", "lwde", "chut", "chud", "chod",
     "gaand", "lund", "bkl", "tmkc", "mc", "bc", "mkl", "sux", "mkc", "lode", "betichod", "badbwe", "lwde", "bhsodike", "boslike", "randi", "rand", "ramdi", "lole", "randi", "haram", "kut", "gandu",
+    "Lauda", "Kuttiya", "Choot", "Bur", "Chudai", "Jhaant", "Lavde", "bhosda", "
     "मादर", "बहन", "भोस", "चूत", "चोद", "चुद",
     "गांड", "लंड", "रंडी", "हराम", "कुत", "गांडू"
 ]
